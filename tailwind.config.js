dd6f7829a7cc5bb01ad3b603ffb3b5e2b9c50d3b
Karen Ascencio <ascencio.karen@yahoo.com.mx>
2021-09-17 18:40:14 -0500

    added color palette to tailwind.config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'plover-blue': '#274C77',
+        'light-blue': '#EDF5FC',
+        'lighter-gray': '#C4C4C4',
+        'darker-gray': '#919390'
+      }
+    },
   },
   variants: {
     extend: {},
