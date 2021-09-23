327fe4253da513825e21f637075b911b2eff5cdb
Hector Hernandez Alcaraz <healcaraz@gmail.com>
2021-09-23 18:43:04 -0500

    asd

--- components/Buttons/index.js
+++ components/Buttons/index.js
@@ -14,8 +14,8 @@ export default function Buttons() {
             <button className="bg-white hover:bg-gray-100 text-plover-blue font-normal m-2 py-2 px-4 border-2 border-plover-blue rounded shadow">
                 Agregar cita
             </button>
-            <button className="bg-white hover:bg-gray-100 text-plover-blue font-normal m-2 py-2 px-4 border-2 border-plover-blue rounded shadow w-8 h-8">
-                IMG
+            <button className="bg-white hover:bg-gray-100 text-plover-blue font-normal text-center m-2 py-2 px-4 border-2 border-plover-blue rounded shadow w-8 h-8">
+            IMG
             </button>
         </>
     )
