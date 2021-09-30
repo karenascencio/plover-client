64a1f23cee3ef2409404bc0f518eea8c23eed572
Hector Hernandez Alcaraz <healcaraz@gmail.com>
2021-09-29 22:01:50 -0500

    Recovery password page

--- components/LoginInput/index.js
+++ components/LoginInput/index.js
@@ -4,10 +4,10 @@ import Image from 'next/image'
 
 export default function LoginInput({imagen}) {
   return(
-  <div className='flex justify-center justify-items-start border-b-2 border-black w-539px' >
+  <div className='flex justify-center justify-items-start border-b-2 border-black w-250px md:w-408px lg:w-539px'  >
     <Image src={imagen} heigth={30} width={30} />
     <input
-      className='w-full py-1 pl-1 border-0 focus:outline-none w-'
+      className='w-full py-1 pl-1 border-0 focus:outline-none'
       type='email'
       placeholder='Correo'
       required
--- pages/recoverPassword.js
+++ pages/recoverPassword.js
@@ -1,26 +1,28 @@
 import React from 'react'
 import H1 from '../components/H1'
 import LoginInput from '../components/LoginInput'
+import LoginButtons from '../components/LoginButtons'
 import email from '../public/email.svg'
 import teeth from '../public/teeth.svg'
 import Image from 'next/image'
+
 export default function RecoverPassword() {
 
     return(
  <>
-  <div className='flex  flex-col justify-center items-center mt-166px'>
-    <div className='mb-70px'>
+  <div className='flex  justify-center flex-col justify-center items-center mt-166px'>
+    <div className='text-center flex mb-70px'>
       <H1
         textTitle='Recupera tu contraseña'
         textColor='plover-blue'
         />
     </div>
-    <div className='grid grid-cols-2 '>
-      <div className=''>
+    <div className='flex justify-center justify-items-center mb-70px'>
+      <div className='invisible sm:visible ml-2' >
        <Image src={teeth} height={65} width={65} />
       </div>
-      <div>
-      <p className=''>
+      <div className='flex justify-center items-center'>
+      <p className='' >
         Ingresa el correo con el que te
         registraste y te enviaremos un
         enlace para crear una nueva
@@ -28,9 +30,10 @@ export default function RecoverPassword() {
       </p>
       </div>
     </div>
-    <div>
+    <div  className='mb-70px w-200px'>
         <LoginInput imagen={email} />
     </div>
+    <LoginButtons title='Cambiar contraseña' />
   </div>
 
 </>
