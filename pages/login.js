aa8c4867e9c2656521755a7576b38c8bccdc93a2
Hector Hernandez Alcaraz <healcaraz@gmail.com>
2021-10-01 21:05:01 -0500

    some error

--- pages/login.js
+++ pages/login.js
@@ -1,31 +1,27 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import { useRouter } from 'next/router'
 import api from '../lib/api'
 // .: Components
 import LoginForm from '../components/LoginForm'
 
-
 export default function Login() {
-  const [userData, setUserData] = useState({ email: '', password: '' })
+  // const [userData, setUserData] = useState({ email: '', password: '' })
   const [error, setError] = useState('')
 
-  const Login = details => {
+  const Login = async details => {
     console.log(details)
-    return details
   }
-
+  
   const buttonHandler = async () => {
-    try{
-    const response = await api.login(Login)
+    try {
+      const response = await api.login(Login)
+      console.log(response)
     }
-    catch(error){console.log(error.message)}
-
+    catch (error) { console.log(error.message) }
   }
-
-
   return (
     <>
-      <LoginForm Login={Login} error={error} buttonHandler={buttonHandler}/>
+      <LoginForm Login={Login} error={error} buttonHandler={buttonHandler} />
     </>
 )
 }
