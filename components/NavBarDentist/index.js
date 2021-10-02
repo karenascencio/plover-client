abbb2c59715661eb32af80e9656db2db792c6457
freddy Castuera <freddycastuera@outlook.es>
2021-10-02 15:34:09 -0600

    terminamos el navbar de dentista

--- components/NavBarDentist/index.js
+++ components/NavBarDentist/index.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import H1 from '../H1'
 import H3 from '../H3'
+import Greeting from '../Greeting'
 import HamburgerMenu from 'react-hamburger-menu'
 import {useState} from 'react'
 import useWindowSize from '../../hooks/useWindowSize'
 import {motion,AnimatePresence} from 'framer-motion'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-
-const forms = [{title:'Informacion general',form:'General Information'},
-                {title:'Antecedentes familiares',form:'Family Background'},
-               {title:'Antecedentes patologicos',form:'Pathological Background'},
-                {title:'Antecedentes no-patologicos',form:'NonPathological Background'},
-               
+import ProfilePicture from '../ProfilePicture'
+const forms = [{title:'Home',form:'General Information'},
+                {title:'Argegar cita',form:'Family Background'},
+               {title:'Consultar cita',form:'Pathological Background'},
+                {title:'Historial clínico',form:'NonPathological Background'},
+                {title:'Historial de pagos',form:'NonPathological Background'},
+                {title:'Configuración',form:'NonPathological Background'},               
             ]
 
 export default function NavBarDentist(props) {
@@ -26,12 +28,15 @@ export default function NavBarDentist(props) {
      
         <div className='sm:sticky top-0 flex flex-row sm:flex-col justify-between sm:justify-start items-center px-080 sm:pt-10 h-20 w-100vw sm:h-100vh sm:w-30vw sm:max-w-sm  bg-plover-blue'>
                 <H1 textTitle='Plover' textColor='white' />
+
+                <ProfilePicture  profilePicture={'https://api.multiavatar.com/jorge%20castuera.png'} />
+                <Greeting userName='mariana'/>
                 
                 
-                <ul className='mt-10 hidden sm:block '>
+                <ul className='mt-10 hidden w-11/12 sm:block'>
                     {
                         forms.map((item,key)=>{
-                            return <li className='mb-10' key={key}><button onClick={(()=>handleForm(item.form))} className='text-white text-xl '>{item.title }</button></li>
+                            return <li className='mb-6' key={key}><Link href={item.form}><a className='bg-white block text-plover-blue text-center text-xl px-10 py-2 rounded-lg'>{item.title}</a></Link></li>
                         })
                     }
                 </ul>
