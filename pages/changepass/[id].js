02268f951b53e01b40a9b3e5a89f97b4e0d1e82d
Hector Hernandez Alcaraz <healcaraz@gmail.com>
2021-10-03 16:22:48 -0500

    test resetpassword

--- pages/changepass/[id].js
+++ pages/changepass/[id].js
@@ -11,6 +11,15 @@ import hidepsw from '../../public/hidepsw.svg'
 import Image from 'next/image'
 
 // .: Take the id from params
+//export const getStaticProps = async (context) => {
+  //const id =  context.params.id
+  //const idUser = await api.resetPassword(id)
+  //return {
+  //  props: {
+  //    idUser
+  //  }
+ // }
+//}
 
 export default function ChangePass() {
   const [seePassword, setSeePassword]= useState(false)
@@ -48,8 +57,6 @@ export default function ChangePass() {
   const buttonHandler = async () => {
     try {
       console.log('handler', resetPassword)
-      const response = await api.resetPassword(resetPassword)
-      console.log(response)
     }
     catch (error) { console.log(error.message) }
   }
