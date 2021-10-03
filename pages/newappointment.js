de84cad970ab2c7ee30deff32f5351741f66fefa
freddy Castuera <freddycastuera@outlook.es>
2021-10-03 17:50:37 -0600

    terminamos la correciones de newappointment

--- pages/newappointment.js
+++ pages/newappointment.js
@@ -3,6 +3,7 @@ import api from '../lib/api'
 import Carrusel from '../components/Carrusel'
 import Calendar from '../components/Calendar'
 import H3 from '../components/H3'
+import H1 from '../components/H1'
 import FormInput from '../components/FormInput'
 import Textarea from '../components/Textarea'
 import PlainText from '../components/PlainText'
@@ -10,15 +11,17 @@ import Toggle from '../components/Toggle'
 import NavBarDentist from '../components/NavBarDentist'
 import { useState,useEffect } from 'react'
 import {useRouter} from 'next/router' 
+import addIcon from '../public/addIcon.svg'
+import Image from 'next/image'
 
 //nota hay un bugsito en el manejo de estado de los toggles
 //corregimos los errores de vercer, corregimos el pull request
 
 const cardsInfo = [
-  { name: 'Alfredo Castuera', procedure: 'Resinas x4', date: '01 septiembre' },
-  { name: 'Anotonio ibarra', procedure: 'Resinas x4', date: '01 septiembre' },
-  { name: 'Hector Hernandez', procedure: 'Resinas x4', date: '01 septiembre' },
-  { name: 'Karen Ascencio', procedure: 'Resinas x4', date: '01 septiembre' }
+  { title: 'Alfredo Castuera', subtitle: 'Resinas x4', thirdTitle: '01 septiembre' },
+  { title: 'Anotonio ibarra', subtitle: 'Resinas x4', thirdTitle: '01 septiembre' },
+  { title: 'Hector Hernandez', subtitle: 'Resinas x4', thirdTitle: '01 septiembre' },
+  { title: 'Karen Ascencio', subtitle: 'Resinas x4', thirdTitle: '01 septiembre' }
 ]
 
 export async function getStaticProps() {
@@ -78,13 +81,22 @@ export default function Newappointment() {
                 {/*el w-full rompe el layout*/}
         		<main className= 'flex w-ful justify-center flex-grow sm:w-65vw mx-11'>
               <div className='max-w-screen-lg w-full flex flex-col items-center'>
-						<div className='self-end '><Calendar value={appointment.date} name={'date'} handleChange={handleChange}/></div>
-						<div className='w-full flex flex-col w-1/2 '>
+                        <Carrusel cards={cardsInfo}/>
+                        <div className='w-full flex justify-between '> 
+                            <H1 textTitle='Cita' textColor='plover-blue'/>
+						    <div className='self-end '><Calendar value={appointment.date} name={'date'} handleChange={handleChange}/></div>
+						</div>
+                        <div className='w-full flex flex-col w-1/2 '>
                             <div className='flex justify-between items-center'>
 							    <div className='self-start '><H3 textTitle='Lista de Procedimientos' textColor='plover-blue'/></div>
-                                <div><button onClick={handleAddProcedure} className='text-white bg-plover-blue w-28 h-30px rounded my-1'>Agregar</button> </div>
+                               
+                                <div><button onClick={handleAddProcedure} className=' flex justify-center text-white bg-plover-blue w-30px sm:w-28  h-30px rounded my-1'>
+                                    <div className='pt-1'><Image src={addIcon} height={15} width={15}/></div>
+                                    <span className='hidden sm:inline-block pl-3 text-sm pt-0.5' >Agregar</span>
+                                    </button> 
+                                </div>
                             </div>
-                            <div className='flex border border-red-500 '>
+                            <div className='flex'>
                             <div className='w-full grid grid-cols-6 gap-x-5'>
 								<div className='col-span-3'><FormInput textLabel='Procedimiento' textName='name' textValue={procedure.name} inputID='Procedimiento' handleChange={handleProcedure} handleBlur={()=>console.log('blur')} /></div>
                                 <div className='col-span-2'><FormInput textLabel='Costo' textName='price' textValue={procedure.price} inputID='Costo' handleChange={handleProcedure} handleBlur={()=>console.log('blur')} /></div>
@@ -97,7 +109,7 @@ export default function Newappointment() {
 											<React.Fragment key={key}>
 											<div className='col-span-3'><PlainText text={procedure.name}/></div>
 											<div className='col-span-2'><PlainText text={procedure.price}/></div>
-                                            <div className=''>
+                                            <div className='flex justify-end'>
                                                 <Toggle id={key}  handleToggle={handleToggle}/>
                                             </div>
 											</React.Fragment>
@@ -125,7 +137,7 @@ export default function Newappointment() {
 
                                 />
                             </div>
-                            <div><button onClick={handleSubmit} className='text-white bg-plover-blue w-28 h-30px rounded my-1'>Enviar</button> </div>
+                            <div><button onClick={handleSubmit} className='text-white text-sm pb-1 bg-plover-blue w-28 h-30px rounded my-1'>Guardar</button> </div>
 						</div>
         </div>
 			</main>
