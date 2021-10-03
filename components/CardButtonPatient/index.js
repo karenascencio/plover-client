b57e9a46ecaadbf5e3890e49339eeebf23eb9bf9
Hector Hernandez Alcaraz <healcaraz@gmail.com>
2021-10-03 00:58:52 -0500

    matching password

--- components/CardButtonPatient/index.js
+++ components/CardButtonPatient/index.js
@@ -5,14 +5,13 @@ import Link from 'next/link'
 export default function CardButtonPatient ({ title, image, reference }) {
   return (
     <Link href={reference}>
-      <a className=' ml-5px md:ml-10px p-1 h-30px item-center rounded hover:bg-light-blue border-plover-blue border-2 '>
+      <div className='flex align-middle justify-center p-1 ml-5px md:ml-10px pb-x h-30px w-30px xl:w-auto item-center rounded hover:bg-light-blue border-plover-blue border-2 '>
 
-        <div className=' lg:hidden items-center object-fill'>
+        <div className=' xl:hidden items-center object-fill'>
           <Image src={image} alt='img' height={15} width={15} />
         </div>
-
-        <a className=' text-center align-middle text-14px sm:hidden lg:block w-30 text-plover-blue' href='#'>{title}</a>
-      </a>
+        <a className='flex align-middle invisible xl:visible text-center lg:object-fit text-14px lg:block w-30 text-plover-blue' href='#'>{title}</a>
+      </div>
     </Link>
   )
 }
--- pages/changepass.js
+++ pages/changepass.js
@@ -16,24 +16,26 @@ export default function ChangePass() {
   const passwordRequirement = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
   
   const newPassword = newCode =>{
-    console.log('contraseña:', newCode)
-    setResetPassword(newCode)
+    //console.log('contraseña:', newCode)
+    //setResetPassword(newCode)
   }
 
   const handlerSubmit = event => {
     event.preventDefault()
 
     const { password, verifyPassword } = resetPassword
-
+    console.log('pass',password)
     console.log('regex', password, passwordRequirement.test(password))
 
-    const matchPassword = password !== verifyPassword
+    const matchPassword = password === verifyPassword
+    console.log('match', matchPassword)
     const requirements = passwordRequirement.test(password)
     
-    if (matchPassword && requirements) {
+    if (matchPassword === false || requirements === false) {
       setDifferentPassword(false)
       console.log('La contraseña no cumple los requisitos')
     } else {
+      console.log('se pudo')
       setDifferentPassword(true)
       newPassword(resetPassword)
     }
@@ -49,60 +51,59 @@ export default function ChangePass() {
   return (
     <>
     <form onSubmit={handlerSubmit}>
-    <div className='flex  justify-center flex-col items-center mt-50px md:mt-166px'>
-      <div className='text-center'>
-        <H1
-          textTitle='Cambia tu contraseña'
-          textColor='plover-blue'
-        />
-      </div>
-      {!differentPassword && <h3>Las contaseñas no son iguales</h3>}
-      <div className=' mb-10px  mt-70px w-200px'>
-        <div className='flex justify-center justify-items-start border-b-2 mb-4 border-black w-250px md:w-408px lg:w-539px'  >
-          <Image src={lock} heigth={40} width={40} />
-          <input
-            className='w-full py-1 pl-1 border-0 focus:outline-none'
-            type={seePassword ? 'text' : 'password' }
-            placeholder='Contraseña'
-            onChange={event => setResetPassword({...resetPassword, password: event.target.value })}
-            value={resetPassword.password}
-            required
+      <div className='flex  justify-center flex-col items-center mt-50px md:mt-166px'>
+        <div className='text-center'>
+          <H1
+            textTitle='Cambia tu contraseña'
+            textColor='plover-blue'
           />
-          <button onClick={()=> setSeePassword(!seePassword)}><Image src={seePassword ? showpsw : hidepsw} heigth={45} width={45} /></button>
         </div>
-      </div>
-
-      <div className=' mb-50px  w-200px'>
-        <div className='flex justify-center justify-items-start border-b-2 mb-4 border-black w-250px md:w-408px lg:w-539px'  >
-          <Image src={lock} heigth={40} width={40} />
-          <input
-            className='w-full py-1 pl-1 border-0 focus:outline-none'
-            type={seePassword ? 'text' : 'password' }
-            placeholder='Contraseña'
-            onChange={event => setResetPassword({...resetPassword, verifyPassword: event.target.value })}
-            value={resetPassword.verifyPassword}
-            required
-          />
-          <button onClick={()=> setSeePassword(!seePassword)}><Image src={seePassword ? showpsw : hidepsw} heigth={45} width={45} /></button>
+        {!differentPassword && <h3>Las contaseñas no son iguales</h3>}
+        <div className=' mb-10px  mt-70px w-200px'>
+          <div className='flex justify-center justify-items-start border-b-2 mb-4 border-black w-250px md:w-408px lg:w-539px'  >
+            <Image src={lock} heigth={40} width={40} />
+            <input
+              className='w-full py-1 pl-1 border-0 focus:outline-none'
+              type={seePassword ? 'text' : 'password' }
+              placeholder='Contraseña'
+              onChange={event => setResetPassword({...resetPassword, password: event.target.value })}
+              value={resetPassword.password}
+              required
+            />
+            <button onClick={()=> setSeePassword(!seePassword)}><Image src={seePassword ? showpsw : hidepsw} heigth={45} width={45} /></button>
+          </div>
         </div>
-      </div>
 
-      <LoginButtons title='Cambiar contraseña' buttonHandler={buttonHandler} />
+        <div className=' mb-50px  w-200px'>
+          <div className='flex justify-center justify-items-start border-b-2 mb-4 border-black w-250px md:w-408px lg:w-539px'  >
+            <Image src={lock} heigth={40} width={40} />
+            <input
+              className='w-full py-1 pl-1 border-0 focus:outline-none'
+              type={seePassword ? 'text' : 'password' }
+              placeholder='Contraseña'
+              onChange={event => setResetPassword({...resetPassword, verifyPassword: event.target.value })}
+              value={resetPassword.verifyPassword}
+              required
+            />
+            <button onClick={()=> setSeePassword(!seePassword)}><Image src={seePassword ? showpsw : hidepsw} heigth={45} width={45} /></button>
+          </div>
+        </div>
+
+        <LoginButtons title='Cambiar contraseña' buttonHandler={buttonHandler} />
         <div className='flex justify-center  mt-4 '>
           <div className='flex invisible sm:visible mr-4 h-auto m-auto'>
             <Image src={teeth} height={65} width={65} />
           </div>
-          <div className=''>
-          <ul className=' list-disc text-14px w-250px text-login-blue'>La contraseña debe contener:
-            <li>Letras mayúsculas y minúsculas</li>
-            <li>Números y signos especiales</li>
-            <li>Tener un mínimo de 8 caracteres</li>
-          </ul>
-        </div>
+          <div className='flex align-middle justify-center'>
+            <ul className=' list-disc text-14px w-250px text-login-blue'>La contraseña debe contener:
+              <li>Letras mayúsculas y minúsculas</li>
+              <li>Números y signos especiales</li>
+              <li>Tener un mínimo de 8 caracteres</li>
+            </ul>
           </div>
-     </div>
-    
-     </form>
+           </div>
+         </div>
+      </form>
     </>
   )
 }
