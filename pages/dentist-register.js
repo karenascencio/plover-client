a46418d3d0e598dfc772b620282b0aa1f61044d9
Hector Hernandez Alcaraz <healcaraz@gmail.com>
2021-10-04 17:43:19 -0500

    Register form yup and formik

--- pages/dentist-register.js
+++ pages/dentist-register.js
@@ -1,21 +1,51 @@
 import React, { useState } from 'react'
+import Image from 'next/image'
 import { useFormik } from 'formik'
 import * as Yup from 'yup'
 
 import api from '../lib/api'
 // .: components
 import RegisterInput from '../components/dentistRegisterInput'
-
+import PasswordInput from '../components/PasswordInput'
+import LoginButtons from '../components/LoginButtons'
+// .: Images
+import lock from '../public/lock.svg'
+import showpsw from '../public/showpsw.svg'
+import hidepsw from '../public/hidepsw.svg'
 
 export default function DentistRegister () {
+  // .: hook for show password
+  const [showPassword, setshowPassword]= useState(false)
+
+  // .: form model formik
   const formik = useFormik({
     initialValues: {
-
-    }
+      name: '',
+      lastName: '',
+      birthDate: '',
+      gender: '',
+      email: '',
+      telephoneNumber: '',
+      clinicName: '',
+      clinicNumber: '',
+      clinicAdress: '',
+      neighborhood: '',
+      zipCode: '',
+      clinicEmail: '',
+      degree: '',
+      college: '',
+      profesionalLicense: '',
+      password: '',
+      verifyPassword: ''
+    },
+    validationSchema: Yup.object({
+      
+    })
   })
+
   return (
     <>
-      <form className='flex justify-center'>
+      <form onSubmit={formik.handleSubmit} className='flex justify-center'>
         <div className='w-280px'>
           <div className='mt-90px mb-50px border-b-2 border-black'>
             <h3 className='text-plover-blue text-2xl'>Datos Personales</h3>
@@ -56,10 +86,10 @@ export default function DentistRegister () {
             inputType='email'
           />
           <RegisterInput
-            textName='phoneNumber'
+            textName='telephoneNumber'
             textLabel='Telefono o celular'
             textValue='change here'
-            inputId='phoneNumber'
+            inputId='telephoneNumber'
             inputType='text'
           />
           <div className=' my-50px border-b-2 border-black'>
@@ -110,7 +140,59 @@ export default function DentistRegister () {
           <div className='border-b-2 border-black'>
             <h3 className='text-plover-blue text-2xl mt-50px'>Datos Profesionales</h3>
           </div>
+          <RegisterInput
+            textName='degree'
+            textLabel='Licenciatura'
+            textValue='change here'
+            inputId='degree'
+            inputType='text'
+          />
+          <RegisterInput
+            textName='college'
+            textLabel='Universidad de egreso'
+            textValue='change here'
+            inputId='college'
+            inputType='text'
+          />
+          <RegisterInput
+            textName='profesionalLicense'
+            textLabel='Cedula profesional'
+            textValue='change here'
+            inputId='clinicEmail'
+            inputType='text'
+          />
+          <div className='border-b-2 border-black'>
+            <h3 className='text-plover-blue text-2xl mt-50px'>Contraseña</h3>
+          </div>
+          <div className=' mb-10px  mt-70px w-280px '>
+            <div className='flex justify-center justify-items-start border-b-2 mb-4 border-black w-280px md:w-408px lg:w-539px'>
+              <Image src={lock} heigth={40} width={40} />
+              <input
+                className='w-full py-1 pl-1 border-0 focus:outline-none'
+                name='password'
+                inputId='password'
+                type={showPassword ? 'text' : 'password' }
+                placeholder='Contraseña'
+              />
+              <button onClick={()=> setshowPassword(!showPassword)}><Image src={showPassword ? showpsw : hidepsw} heigth={45} width={45} /></button>
+            </div>
+          </div>
+
+          <div className=' mb-50px  w-280px'>
+            <div className='flex justify-center justify-items-start border-b-2 mb-4 border-black w-280px md:w-408px lg:w-539px'  >
+              <Image src={lock} heigth={40} width={40} />
+              <input
+                className='w-full py-1 pl-1 border-0 focus:outline-none'
+                name='verifyPassword'
+                inputId='verifyPassword'
+                type={showPassword ? 'text' : 'password' }
+                placeholder='Reingresa la contraseña'
+              />
+              <button onClick={()=> setshowPassword(!showPassword)}><Image src={showPassword ? showpsw : hidepsw} heigth={45} width={45} /></button>
+            </div>
+          </div>
         </div>
+        
       </form>
     </>
   )
