2875d12ca1609d98fdc7d854b8fb8a5a7c6aeeff
Karen Ascencio <ascencio.karen@yahoo.com.mx>
2021-10-09 03:44:18 -0500

    added more changes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import SuccessAlert from '../components/SuccessAlert'
 
 export default function index () {
   return (
@@ -7,18 +6,6 @@ export default function index () {
       <div className='text-plover-blue text-9xl'>
         Welcome to Plover!
       </div>
-      <SuccessAlert
-        textAlert='Tu información fue actualizada correctamente.'
-        status='¡Éxito!'
-        mainColor='plover-blue'
-        bgColor='light-blue'
-      />
-      <SuccessAlert
-        textAlert='Tu información no fue actualizada correctamente, intenta de nuevo.'
-        status='¡Error!'
-        mainColor='red-700'
-        bgColor='red-100'
-      />
     </>
   )
 }
