595a3766292d359acc59bdfc9f42d9cdb29c501b
freddy Castuera <freddycastuera@outlook.es>
2021-10-13 11:12:05 -0600

    alertas de pago y subida de comprobante listas

--- pages/alerts.js
+++ pages/alerts.js
@@ -4,15 +4,26 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 export default function Alerts(){
-  const notify = () => toast.success('Pago agregado', {
-    position: "top-right",
-    autoClose: 5000,
-    hideProgressBar: true,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    });
+  const resolveWithSomeData = new Promise(resolve => setTimeout(() => resolve("Subiendo comprobante"), 3000));
+  const notify = () =>toast.promise(
+      resolveWithSomeData,
+      {
+        pending: 'Promise is pending',
+        success: {
+          render({data}){
+            return `Hello ${data}`
+          },
+          // other options
+          icon: "🟢",
+        },
+        error: {
+          render({data}){
+            // When the promise reject, data will contains the error
+            return <MyErrorComponent message={data.message} />
+          }
+        }
+      }
+  )
 
   return (
       <>
